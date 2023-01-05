a56ee32b25ea79806cb0b542095d4d9781b78ca1
Saoud Rana <srana@credly.com>
2023-01-05 12:39:23 -0800

    adding code for first step

--- day1/index.js
+++ day1/index.js
@@ -35,3 +35,8 @@
 // In case the Elves get hungry and need extra snacks, they need to know which Elf to ask: they'd like to know how many Calories are being carried by the Elf carrying the most Calories. In the example above, this is 24000 (carried by the fourth Elf).
 
 // Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?
+
+const fs = require('fs')
+const input = fs.readFileSync('./input.txt').toString().split("\n");
+//console.log(input)
+
